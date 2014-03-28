090aa5b8a13b340d04cf56dce7f548cf65f3b6c2
Jack Henahan <jhenahan@me.com>
2014-03-28 13:55:03 -0400

    Reduce undo-tree pollution

--- custom.el
+++ custom.el
@@ -5,7 +5,8 @@
  ;; If there is more than one, they won't work right.
  '(LaTeX-command "latex -shell-escape")
  '(TeX-PDF-mode t)
- '(custom-raised-buttons t))
+ '(custom-raised-buttons t)
+ '(undo-tree-history-directory-alist (quote (("." . "~/.emacs.d/undofiles")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
