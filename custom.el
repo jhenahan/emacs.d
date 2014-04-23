b6ad4853779f3c982cc70d0cfd0b193c69a41f98
Jack Henahan <jhenahan@me.com>
2014-04-22 23:40:06 -0400

    Raised buttons
    For some reason, cus-edit unsets the usual raised buttons in
    Customize screens.

--- custom.el
+++ custom.el
@@ -4,10 +4,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(LaTeX-command "latex -shell-escape")
- '(TeX-PDF-mode t))
+ '(TeX-PDF-mode t)
+ '(custom-raised-buttons t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-) 
+ )
+ 
