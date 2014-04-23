ccf06c806a259328449a930f4bc2ca12a09fd01d
Jack Henahan <jhenahan@me.com>
2014-04-22 23:20:45 -0400

    Clean up custom

--- custom.el
+++ custom.el
@@ -4,12 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(LaTeX-command "latex -shell-escape")
- '(TeX-PDF-mode t)
- '(custom-raised-buttons t)
- '(undo-tree-history-directory-alist (quote (("." . "~/.emacs.d/undofiles")))))
+ '(TeX-PDF-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(shm-current-face ((t (:background "#525252"))) t))
+) 
