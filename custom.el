97d537a396a8e8b962428023c8672ca83d67bccf
Jack Henahan <jhenahan@me.com>
2014-09-23 09:33:24 -0400

    Themes

--- custom.el
+++ custom.el
@@ -5,7 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(LaTeX-command "latex -shell-escape")
  '(TeX-PDF-mode t)
- '(custom-raised-buttons t))
+ '(custom-raised-buttons t)
+ '(custom-safe-themes (quote ("3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" "025354235e98db5e7fd9c1a74622ff53ad31b7bde537d290ff68d85665213d85" "6fe6ab4abe97a4f13533e47ae59fbba7f2919583f9162b440dd06707b01f7794" default)))
+ '(sml/name-width 12)
+ '(sml/theme (quote dark)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
